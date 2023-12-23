9fccf97f7d58c3365940ca0b5e44eb0d5aa37cdc
ZxcBody <153951353+ZxcBody@users.noreply.github.com>
2023-12-23 23:51:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 class Car:
-    def __init__(self, motor):
+    def __init__(self, motor = '8v', tank_capacity = 24, leigth = 240, place = 5):
         self.motor = motor
+        self.tank_capacity = tank_capacity
+        self.leight = leigth
+        self.place = place
 
 
 class Bus(Car):
-    def __init__(self, motor, tank_capacity, leigth, place):
-        super().__init__(motor)
-        self.leight = leigth
-        self.place = place
-        self.tank_capacity = tank_capacity
-        self. place = place
+    def __init__(self):
+        super().__init__()
+        self.tank_capacity = 48
+        self.leight = 330
+        self.place = 20
 
     def __str__(self):
-        return print(f"Об'єм баку {self.tank_capacity}\nДовжина {self.leight}\n Мотор {self.motor}\nМісць {self.place}")
+        return f"Об'єм баку-{self.tank_capacity}\nДовжина-{self.leight}\nМотор-{self.motor}\nМісць-{self.place}"
+
+bus = Bus()
+print(bus)
